127352bf625b4a94a29aef128d4ae1abbde5fc0d
jerry wick <aaaaa20021029@gmail.com>
2021-04-28 03:42:14 +0800

    add header

--- products.py
+++ products.py
@@ -9,3 +9,13 @@ print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+# write 寫入檔案
+# 創造出"products.txt"
+# 有不同屬性(名稱, 價格)的時候常常用 .csv
+# 可以用excel打開
+with open ('products.csv', 'w') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+# +可以把字串跟字串合再一起
